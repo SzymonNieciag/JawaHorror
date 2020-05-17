c997007b81a30c60da4de1d9119fe5917ca3786c
DESKTOP-2S6IUD4\nieci <nieciagszymon@gmail.com>
2020-05-17 18:10:42 +0200

    poczatek przygody

---
+++ Jawa/Source/Jawa/JawaGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JawaGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AJawaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AJawaGameMode();
+};
+
+
+
